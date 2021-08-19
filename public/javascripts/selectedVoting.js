5a3e1f36588509dd7f3ffbaf018e21e05c21da6d
Hayun <mycolki@gmail.com>
2021-08-19 22:40:17 +0900

    Feat: change selectedVoting post js logic

--- public/javascripts/selectedVoting.js
+++ public/javascripts/selectedVoting.js
@@ -1,14 +1,30 @@
-// const votingOptions = document.querySelectorAll('.votingOption');
+const votingOptions = document.querySelectorAll('.votingOption');
 
-// function preventBtnClick() {
-//   for (const option of votingOptions) {
-//     option.setAttribute('disabled', true);
-//   }
-// }
+function preventBtnClick() {
+  for (const option of votingOptions) {
+    option.setAttribute('disabled', true);
+  }
+}
 
-// for (const option of votingOptions) {
+for (const option of votingOptions) {
+  option.addEventListener('click', () => {
+    option.classList.add('selected');
+    setTimeout(() => {
+      preventBtnClick();
+    })
+  });
+}
+
+// function submitSelectedVoting() {
+//   console.log('dd')
 //   option.addEventListener('click', () => {
 //     option.classList.add('selected');
-//     return preventBtnClick();
+//     preventBtnClick();
 //   });
 // }
+
+// const selectedVotingForm = document.querySelector('.selectedVotingForm');
+// selectedVotingForm.addEventListener('submit', () => {
+//   console.log('d')
+// });
+
