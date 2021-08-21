6af39e83019725e8ffde8bf31945acda791e89a7
Hayun <mycolki@gmail.com>
2021-08-22 00:22:51 +0900

    Feat: add login post test

--- spec/login.js
+++ spec/login.js
@@ -1,19 +1,33 @@
-const supertest = require('supertest');
+const request = require("supertest");
+const { expect } = require("chai");
+const app = require("../app");
 
-const request = require('supertest');
-const { expect } = require('chai');
-const app = require('../app');
+describe("login_ GET", function () {
+  const LOGIN_VIEWER_TEXT = "Welcome to Blue Vote";
 
-describe("login render", function () {
   it("should respond with login template", function (done) {
     request(app)
       .get("/login")
       .expect(200)
       .expect("Content-Type", /html/)
-      .end((err, res) => {
+      .end(function (err, res) {
         if (err) return done(err);
 
-        expect(res.text).to.include("Welcome to Blue Vote");
+        expect(res.text).to.include(LOGIN_VIEWER_TEXT);
+        done();
+      });
+  });
+});
+
+describe("login_ POST", function () {
+  it("should respond with main template", function (done) {
+    request(app)
+      .post("/login")
+      .expect(302)
+      .expect("Location", "/")
+      .end(function (err, res) {
+        if (err) return done(err);
+
         done();
       });
   });
